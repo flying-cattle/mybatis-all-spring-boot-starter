216867a71cd0f0a855af28495d222f609d34fd2b
Administrator <Administrator@L9UXKQWF6GXVADJ>
2018-06-19 23:29:48 +0800

    基础提交

---
+++ src/main/java/com/github/flyingcattle/swagger/SwaggerAutoConfiguration.java
@@ -0,0 +1,48 @@
+package com.github.flyingcattle.swagger;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.github.flyingcattle.swagger.properties.SwaggerProperties;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.configuration.Swagger2DocumentationConfiguration;
+
+@Configuration
+@ConditionalOnProperty(value = "spring.swagger.enabled", matchIfMissing = true)
+@Import({Swagger2DocumentationConfiguration.class})
+public class SwaggerAutoConfiguration{
+	
+	@Bean
+    @ConditionalOnMissingBean
+    public SwaggerProperties swaggerProperties() {
+        return new SwaggerProperties();
+    }
+	
+	@Bean
+	@ConditionalOnMissingBean
+    public Docket createRestApi(SwaggerProperties swaggerProperties) {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo(swaggerProperties))
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo(SwaggerProperties swaggerProperties) {
+        return new ApiInfoBuilder()
+                .title(swaggerProperties.getTitle())
+                .description(swaggerProperties.getDescription())
+                .termsOfServiceUrl(swaggerProperties.getTermsOfServiceUrl())
+                .version(swaggerProperties.getVersion())
+                .build();
+    }
+}
---
+++ src/main/java/com/github/flyingcattle/swagger/properties/SwaggerProperties.java
@@ -0,0 +1,41 @@
+package com.github.flyingcattle.swagger.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Data;
+
+@Data
+@ConfigurationProperties("spring.swagger")
+public class SwaggerProperties {
+	
+	private static final String DEFAULT_NULL = "";
+	/**
+     * swagger会解析的包路径
+     */
+    private String basePackage = DEFAULT_NULL;
+    
+    /**
+     * 标题
+     */
+    private String title = DEFAULT_NULL;
+    
+    /**
+     * 版本
+     */
+    private String version = DEFAULT_NULL;
+    
+    /**
+     * 服务条款URL
+     */
+    private String termsOfServiceUrl = DEFAULT_NULL;
+    
+    /**
+     * 描述
+     */
+    private String description = DEFAULT_NULL;
+    
+    /**
+     * 是否使用
+     */
+    private boolean enabled = false;
+}
